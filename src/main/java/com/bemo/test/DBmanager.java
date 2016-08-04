00461214e61e2e4a1e182c4665c62a2bf9cfc401
PBesze <pbesze@gmail.com>
2016-08-04 22:29:41 +0200

    Update DBmanager.java

--- src/main/java/com/bemo/test/DBmanager.java
+++ src/main/java/com/bemo/test/DBmanager.java
@@ -4,8 +4,34 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 
+import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.faces.bean.SessionScoped;
+import javax.faces.event.ValueChangeEvent;
+
 @ManagedBean(name = "DBmanager", eager = true)
-@RequestScoped
-public void DBmanager (ActionEvent e){
-   data="Hello World";
+@SessionScoped
+public class DBmanager implements Serializable {
+
+private static final long serialVersionUID = 1L;
+
+   private String data = "sample data";
+	
+   public String showResult(){
+      return "result";
+   }
+
+   public void updateData(ActionEvent e){
+      data="Hello World";
+   }
+
+   public String getData() {
+      return data;
+   }
+
+   public void setData(String data) {
+      this.data = data;
+   }
 }
