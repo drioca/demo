30edaaa3c9e311d1985de3698db125abb8556591
PBesze <pbesze@gmail.com>
2016-08-05 13:13:57 +0200

    Update DBmanager.java

--- src/main/java/com/bemo/test/DBmanager.java
+++ src/main/java/com/bemo/test/DBmanager.java
@@ -27,7 +27,7 @@ private static Connection getConnection() throws URISyntaxException, SQLExceptio
    public void insert () {
       
       UserData.age += 3;
-      private String sqlExp = "INSERT INTO test (age) VALUES (" + "3" +");";
+      private String sqlExp () = "INSERT INTO test (age) VALUES (" + "3" +");";
         try {
             Connection con = getConnection( );
             Statement stmt = con.createStatement();
