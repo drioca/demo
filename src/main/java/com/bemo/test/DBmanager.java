30091916c3f9c5b31e17c90eb6c9685b56f70f3e
PBesze <pbesze@gmail.com>
2016-08-05 10:07:46 +0200

    Update DBmanager.java

--- src/main/java/com/bemo/test/DBmanager.java
+++ src/main/java/com/bemo/test/DBmanager.java
@@ -1,13 +1,17 @@
 package com.bemo.test;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
-
 import java.io.Serializable;
-
 import javax.faces.bean.SessionScoped;
 import javax.faces.event.ValueChangeEvent;
+//DB connection
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @ManagedBean(name = "DBmanager", eager = true)
 @SessionScoped
@@ -19,6 +23,32 @@ public static void main (){
    public void insert () {
       
       UserData.age = 40;
+      
+          get("/db", (req, res) -> {
+      Connection connection = null;
+      Map<String, Object> attributes = new HashMap<>();
+      try {
+        connection = DatabaseUrl.extract().getConnection();
+
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+        ArrayList<String> output = new ArrayList<String>();
+        while (rs.next()) {
+          output.add( "Read from DB: " + rs.getTimestamp("tick"));
+        }
+
+        attributes.put("results", output);
+        return new ModelAndView(attributes, "db.ftl");
+      } catch (Exception e) {
+        attributes.put("message", "There was an error: " + e);
+        return new ModelAndView(attributes, "error.ftl");
+      } finally {
+        if (connection != null) try{connection.close();} catch(SQLException e){}
+      }
+    }, new FreeMarkerEngine());
    }
 
    
