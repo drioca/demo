632626cc98b6f8fdca371f38ea607810c1a34bda
Peter Besze <pbesze@gmail.com>
2016-08-08 22:39:13 +0200

    Update DBmanager.java

--- src/main/java/com/bemo/test/DBmanager.java
+++ src/main/java/com/bemo/test/DBmanager.java
@@ -36,7 +36,7 @@ private static Connection getConnection() throws URISyntaxException, SQLExceptio
         try {
             Connection con = getConnection( ); 
             Statement stmtc = con.createStatement();
-         stmtc.executeUpdate("CREATE TABLE IF NOT EXSISTS public.test (key_column bigserial NOT NULL, age numeric(4,0), ctimestamp timestamptz DEFAULT now(), CONSTRAINT test_pkey PRIMARY KEY (key_column) );");
+         stmtc.executeUpdate("CREATE TABLE IF NOT EXSISTS test (key_column bigserial NOT NULL, age numeric(4,0), ctimestamp timestamptz DEFAULT now(), CONSTRAINT test_pkey PRIMARY KEY (key_column) );");
         Statement stmt = con.createStatement();
         stmt.executeUpdate(iNs);
         stmt.executeUpdate("commit;");
