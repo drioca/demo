e51ac7b36e5bb6ae4336e96549984395f7c9588a
Peter Besze <pbesze@gmail.com>
2016-08-10 23:29:10 +0200

    Update DBmanager.java

--- src/main/java/com/bemo/test/DBmanager.java
+++ src/main/java/com/bemo/test/DBmanager.java
@@ -109,7 +109,7 @@ Record record = new Record();
 record.setKey_column(rs.getLong("key_column"));
 record.setAge(rs.getInt("age"));
 //String cts = (rs.getString("ctimestamp"));
-record.setCtimestampt(rs.getString("ctimestamp"));
+record.setCtimestamp(rs.getString("ctimestamp"));
 list.add(record);
 } 
 con.close();
