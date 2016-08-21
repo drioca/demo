c1bdb201747e4406d4d7aa9de0787d940fa9bad4
Peter Besze <pbesze@gmail.com>
2016-08-21 21:29:28 +0200

    Update NavigationController.java

--- src/main/java/com/bemo/test/NavigationController.java
+++ src/main/java/com/bemo/test/NavigationController.java
@@ -48,5 +48,17 @@ public class NavigationController implements Serializable {
       this.pageId = pageId;
    }
 
+   public String showPage(){
+      if(pageId == null){
+         return "home";
+      }
+      if(pageId.equals("1")){
+         return "page1";
+      }else if(pageId.equals("2")){
+         return "page2";
+      }else{
+         return "home";
+      }
+   }
 
 }
