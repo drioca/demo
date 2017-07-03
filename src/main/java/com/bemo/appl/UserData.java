bae3a89533dcbe99db2c20229ba49082439e19a7
Peter Besze <pbesze@gmail.com>
2017-07-03 13:54:12 +0200

    Update UserData.java

--- src/main/java/com/bemo/appl/UserData.java
+++ src/main/java/com/bemo/appl/UserData.java
@@ -140,7 +140,7 @@ private static final long serialVersionUID = 1L;
       	return avg;
    	}
   public static void setAverage(String newavg) {
-      	avg = newavg;
+      	avg = newavg.subString(0,4);
 	}
 
 }
